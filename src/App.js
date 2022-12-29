a632343b5e6c78b26e288f1505f1e3846d4830ec
Carlopsd <carlopsd@gmail.com>
2022-12-28 20:43:39 -0600

    Agrego componente LED

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Led from './components/led';
+import BigLed from './components/big-led';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section class="left-part">
+      <div class="leds">
+        <BigLed/>
+        <Led/>
+        <Led/>
+        <Led/>
+      </div>
+    </section>
   );
 }
 
